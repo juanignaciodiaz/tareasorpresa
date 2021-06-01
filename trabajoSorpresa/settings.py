bb221163b0f4b248ddadf35da675c424f443d4f9
ReneRetamales <re.retamales@duocuc.cl>
2021-06-01 17:44:31 -0400

    Modificaciones y conexión de css

--- trabajoSorpresa/settings.py
+++ trabajoSorpresa/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'trabajoSorpresa',
         'USER': 'root',
-        'PASSWORD': 'admin',
+        'PASSWORD': '12341234',
         'HOST': 'localhost',
         'PORT': '3306',
     }
