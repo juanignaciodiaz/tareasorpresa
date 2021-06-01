7895e2f19551681962775298ea65fe800f7a258e
Camilo_Ts <camil.tapias@gmail.com>
2021-06-01 09:30:09 -0400

    Django configurado para el proyecto'

---
+++ tareaSorpresa/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Nota(models.Model):
+
+    titulo = models.CharField('Titulo nota:', max_length=50, blank=False, null=False)
+    descripcion = models.CharField('Descripcion:', max_length=150, blank=False, null=False)
+    fecha_creacion = models.DateTimeField('Fecha creacion:', auto_now_add=True)
+    fecha_finalizacion = models.DateTimeField('Fecha finalizacion:', auto_now=True)
+
+    
---
+++ trabajoSorpresa/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for trabajoSorpresa project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trabajoSorpresa.settings')
+
+application = get_asgi_application()
