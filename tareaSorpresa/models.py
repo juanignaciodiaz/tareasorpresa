bae5ef6f7e45322e082e74187cb29c0ac7b13638
Camilo_Ts <camil.tapias@gmail.com>
2021-06-01 09:35:53 -0400

    Actualizacion del models.py

--- tareaSorpresa/models.py
+++ tareaSorpresa/models.py
@@ -8,5 +8,5 @@ class Nota(models.Model):
     descripcion = models.CharField('Descripcion:', max_length=150, blank=False, null=False)
     fecha_creacion = models.DateTimeField('Fecha creacion:', auto_now_add=True)
     fecha_finalizacion = models.DateTimeField('Fecha finalizacion:', auto_now=True)
+    estado = models.CharField('Estado:', max_length=30, blank=True, null=True)
 
-    
