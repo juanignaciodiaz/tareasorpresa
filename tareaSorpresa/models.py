95e5dfb3d70b6449fae9aee3dad5d64358707e35
MiltonB <milton1villa23@gmail.com>
2021-06-01 16:24:07 -0400

    agregar cosas

--- tareaSorpresa/models.py
+++ tareaSorpresa/models.py
@@ -7,6 +7,6 @@ class Nota(models.Model):
     titulo = models.CharField('Titulo nota:', max_length=50, blank=False, null=False)
     descripcion = models.CharField('Descripcion:', max_length=150, blank=False, null=False)
     fecha_creacion = models.DateTimeField('Fecha creacion:', auto_now_add=True)
-    fecha_finalizacion = models.DateTimeField('Fecha finalizacion:', auto_now=True)
-    estado = models.CharField('Estado:', max_length=30, blank=True, null=True)
+    fecha_finalizacion = models.DateTimeField('Fecha finalizacion:')
+    estado = models.BooleanField('Estado:')
 
