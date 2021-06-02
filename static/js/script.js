3fee9c2d7ac0b3dba7619e90fbd198a9162022a5
Camilo_Ts <camil.tapias@gmail.com>
2021-06-02 00:11:28 -0400

    prueba

---
+++ static/js/script.js
@@ -0,0 +1,14 @@
+(function(d, w) {
+    w.addEventListener('resize', function() {
+        const tabla = document.getElementById('tabla');
+        // console.log(tabla);
+        if (screen.width < 800) {
+            // console.log(tabla);
+            // console.log('hola');
+            tabla.classList.toggle('table-responsive')
+            
+        } else {
+            console.log(tabla.classList);
+        }
+    })
+})(document, window)
