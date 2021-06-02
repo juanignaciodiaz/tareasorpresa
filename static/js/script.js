13c51e1910684d851b587eee76cd88d72ffd3f79
Camilo_Ts <camil.tapias@gmail.com>
2021-06-02 01:13:48 -0400

    Se agrego boton de volver en la pagina de agregar nota

--- static/js/script.js
+++ static/js/script.js
@@ -2,13 +2,26 @@
     w.addEventListener('resize', function() {
         const tabla = document.getElementById('tabla');
         // console.log(tabla);
-        if (screen.width <= 800) {
+        if (screen.width == 800 || screen.width == 360 || screen.width == 411 || screen.width == 320 || screen.width == 368) {
             // console.log(tabla);
-            // console.log('hola');
-            tabla.classList.toggle('table-responsive')
-            
-        } else {
             console.log(tabla.classList);
+            for (const i in tabla.classList) {
+                if (tabla.classList[i] == 'table-responsive') {
+                    
+                } else {
+                    tabla.classList.toggle = 'table-responsive';
+                }
+            }
+            // tabla.classList.toggle('table-responsive')
+            
+            
+        } else if (screen.width > 800) {
+            console.log('Soy mayor a 800');
+            for (const i in tabla.classList) {
+                if (tabla.classList[i] == 'table-responsive') {
+                    tabla.classList.toggle = 'table-responsive';
+                }
+            }
         }
     })
 })(document, window)
